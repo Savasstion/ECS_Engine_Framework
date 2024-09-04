cc4614a3d2b2a7dcb7c7156a5b30a814be658339
LexPolka <lexthepolka@gmail.com>
2024-09-04 18:49:52 +0800

    cum

--- Prac Game Prog/Systems/Core/Physics.cpp
+++ Prac Game Prog/Systems/Core/Physics.cpp
@@ -3,6 +3,7 @@
 void Physics::DoScenePhysics(std::shared_ptr<Scene> scene)
 {
     DoAllCycleOfMotion(scene->componentManager->GetComponents(RIGIDBODY2D));
+    HandleCollision(scene->componentManager->GetComponents(BOX_COLLIDER));
 }
 
 //For Rigidbody 2D
@@ -20,8 +21,10 @@ void Physics::DoCycleOfMotion(std::shared_ptr<Rigidbody2DComponent> rgb)
     rgb->velocity += rgb->acceleration;
     rgb->velocity *= (1 - rgb->friction);
 
-    if (rgb->velocity > rgb->maxVelocity)
-        rgb->velocity = rgb->maxVelocity;
+    if (rgb->velocity.x > rgb->maxVelocity.x)
+        rgb->velocity.x = rgb->maxVelocity.x;
+    if (rgb->velocity.y > rgb->maxVelocity.y)
+        rgb->velocity.y = rgb->maxVelocity.y;
 
     if (rgb->parent->transform != nullptr)
         rgb->parent->transform->position += rgb->velocity;
@@ -38,4 +41,47 @@ void Physics::HandleCollision(std::vector<std::shared_ptr<Component>> colliders)
         
     }
     
+}
+
+void Physics::HandleCollision(std::vector<std::shared_ptr<Component>> colliders) {
+    // Brute force collision detection (improve with spatial partitioning for large numbers of objects)
+    for (size_t i = 0; i < colliders.size(); ++i) {
+        for (size_t j = i + 1; j < colliders.size(); ++j) {
+            auto colliderA = std::dynamic_pointer_cast<BoxColliderComponent>(colliders[i]);
+            auto colliderB = std::dynamic_pointer_cast<BoxColliderComponent>(colliders[j]);
+
+            if (CheckCollision(colliderA, colliderB)) {
+                auto rbA = std::dynamic_pointer_cast<Rigidbody2DComponent>(colliderA->parent->GetComponent(RIGIDBODY2D));
+                auto rbB = std::dynamic_pointer_cast<Rigidbody2DComponent>(colliderB->parent->GetComponent(RIGIDBODY2D));
+                ResolveCollision(rbA, rbB);
+            }
+        }
+    }
+}
+
+bool Physics::CheckCollision(std::shared_ptr<BoxColliderComponent> a, std::shared_ptr<BoxColliderComponent> b) {
+    // Simple AABB (Axis-Aligned Bounding Box) collision detection
+    return (a->box.right >= b->box.left &&
+        a->box.left <= b->box.right &&
+        a->box.bottom >= b->box.top &&
+        a->box.top <= b->box.bottom);
+}
+
+void Physics::ResolveCollision(std::shared_ptr<Rigidbody2DComponent> rbA, std::shared_ptr<Rigidbody2DComponent> rbB) {
+    // Basic collision resolution - inverse the velocity or separate the objects
+    if (rbA && rbB) {
+        // Elastic collision response (simplified)
+        D3DXVECTOR2 normal = rbB->parent->transform->position - rbA->parent->transform->position;
+        D3DXVec2Normalize(&normal, &normal);
+
+        float relativeVelocity = D3DXVec2Dot(&(rbB->velocity - rbA->velocity), &normal);
+        float e = 1.0f;  // Coefficient of restitution (1 = perfectly elastic, 0 = perfectly inelastic)
+
+        float j = -(1 + e) * relativeVelocity / (1 / rbA->mass + 1 / rbB->mass);
+
+        D3DXVECTOR2 impulse = j * normal;
+
+        rbA->velocity -= impulse / rbA->mass;
+        rbB->velocity += impulse / rbB->mass;
+    }
 }
--- Prac Game Prog/Systems/Core/Physics.h
+++ Prac Game Prog/Systems/Core/Physics.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <vector>
 #include  "../../Components/Rigidbody2DComponent.h"
+#include "../../Components/BoxColliderComponent.h"
 #include "../../BaseClasses/Scene.h"
 
 class Physics
@@ -14,4 +15,8 @@ public:
     static void DoAllCycleOfMotion(std::vector<std::shared_ptr<Component>> rbg2DComponents);    //  for 2D
     static void DoCycleOfMotion(std::shared_ptr<Rigidbody2DComponent> rgb);
     static void HandleCollision(std::vector<std::shared_ptr<Component>> collider);
+
+private:
+    static bool CheckCollision(std::shared_ptr<BoxColliderComponent> a, std::shared_ptr<BoxColliderComponent> b);
+    static void ResolveCollision(std::shared_ptr<Rigidbody2DComponent> rbA, std::shared_ptr<Rigidbody2DComponent> rbB);
 };
