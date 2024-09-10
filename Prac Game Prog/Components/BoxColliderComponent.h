2e11cc8952de41d5ffcea3648c90b5eb681f4c23
Savasstion <savasstion1108@gmail.com>
2024-09-10 12:25:52 +0800

    Collider updates

--- Prac Game Prog/Components/BoxColliderComponent.h
+++ Prac Game Prog/Components/BoxColliderComponent.h
@@ -1,14 +1,15 @@
 #pragma once
 #include "ColliderComponent.h"
 #include "d3d9.h"
+#include "../Enums/ColliderTag.h"
 
 //  Greg's Collider
 //  TODO: get this working one day
-enum class ColliderTag {
-    Player,
-    Enemy,
-    PowerUp,
-};
+// enum class ColliderTag {
+//     Player,
+//     Enemy,
+//     PowerUp,
+// };
 
 class BoxColliderComponent : public ColliderComponent
 {
@@ -16,7 +17,7 @@ public:
     RECT box;
     ColliderTag tag;
     BoxColliderComponent();
-    BoxColliderComponent(RECT box, ColliderTag tag = ColliderTag::Player);
+    BoxColliderComponent(RECT box, ColliderTag tag = ColliderTag::DEFAULT);
     void SetBox(RECT box, D3DXVECTOR2 centerPos);
     void SetColliderTag(ColliderTag tag){this->tag = tag;}
     void UpdateColliderPos(D3DXVECTOR2 velocity) override;
--- Prac Game Prog/Components/ColliderComponent.h
+++ Prac Game Prog/Components/ColliderComponent.h
@@ -7,6 +7,11 @@
 class ColliderComponent : public Component
 {
 public:
+    D3DXVECTOR2 relativePos = D3DXVECTOR2(0,0);
+    
     virtual ~ColliderComponent() = default;  // Virtual destructor
-    virtual void UpdateColliderPos(D3DXVECTOR2 velocity);
+    virtual void UpdateColliderPos(D3DXVECTOR2 parentPos);
+
+    void SetRelativePosition(D3DXVECTOR2 pos){this->relativePos = pos;}
+    D3DXVECTOR2 GetRelativePosition(){return this->relativePos;}
 };
