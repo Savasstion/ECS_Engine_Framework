79ca103ea09ac123c7e79b784f7873d638b7263d
Savasstion <savasstion1108@gmail.com>
2024-09-15 20:49:19 +0800

    Option scene globals

---
+++ Prac Game Prog/Scenes/SceneGlobals/OptionSceneGlobals.cpp
@@ -0,0 +1,4 @@
+#include "OptionSceneGlobals.h"
+
+float volumeModifier = 1.0f;
+std::shared_ptr<SceneManager> optionSceneManager;
---
+++ Prac Game Prog/Scenes/SceneGlobals/OptionSceneGlobals.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <memory>
+
+#include "../../Systems/Managers/SceneManager.h"
+
+extern float volumeModifier;
+extern std::shared_ptr<SceneManager> optionSceneManager;
